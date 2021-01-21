aabe2c330b4e2277fd4b8594d97f92cd6d91a7d2
Felix Harz <felixharz99@gmail.com>
2021-01-21 12:33:43 +0100

    comments added

--- js/userInterfaceOnLoad.js
+++ js/userInterfaceOnLoad.js
@@ -200,3 +200,16 @@ function createCardZiele(angemeldeterUser){
 
     return false;
 }
+
+
+function toggleRows(){
+    let rows = document.getElementById("tabelle0").getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    for (i = 0; i < rows.length; i++) {
+        rows[i].addEventListener('click', function() {
+            this.classList.toggle('markiert');
+        });
+}
+
+/*function checkFortschritt(){
+
+}*/
