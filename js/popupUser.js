0819ec61e0b2c2647605fa9a28111b4eeb6e3a18
Yannick Stumpf <yannickstumpf17@gmail.com>
2021-01-22 14:25:56 +0100

    rest

--- js/popupUser.js
+++ js/popupUser.js
@@ -28,3 +28,33 @@ function ModalneuesHauptzielHinzufügen(){
         }
     }
 }
+
+document.getElementById("btnUnterzielAdd").addEventListener("click", ModalneuesUnterzielHinzufügen());
+
+function ModalneuesUnterzielHinzufügen(){
+    
+    let modalNewHauptziel = document.getElementById("newHauptziel");
+
+    // Get the button that opens the modal
+    let btn = document.getElementById("btnUnterzielAdd");
+
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("closeUnterziel")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modalNewHauptziel.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modalNewHauptziel.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modalNewHauptziel) {
+            modalNewHauptziel.style.display = "none";
+        }
+    }
+}
